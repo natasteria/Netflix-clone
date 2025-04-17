85c5442487332bc2de44b4bbcd81e25cdef130dd
natasteria <173689548+natasteria@users.noreply.github.com>
2025-04-17 15:15:14 +0200

    feat: add scroll arrows to trending section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
+// trending-Section 
+let trendingRow = document.getElementById('trending-row');
+let leftArrow = document.getElementById('arrow-left');
+let rightArrow = document.getElementById('arrow-right');
+
+console.log(trendingRow);
+
+rightArrow.addEventListener("click", function(){
+    trendingRow.scrollBy({
+        left: 1000, 
+        behavior: "smooth"
+    })
+    leftArrow.classList.remove("d-none");
+
+})
+
+leftArrow.addEventListener("click", function(){
+    trendingRow.scrollBy({
+        left: -1000, 
+        behavior: "smooth"
+    })
+    leftArrow.classList.add("d-none");
+
+})
+
 // FAQ 
 
 let faqItem = document.getElementsByClassName('faq-item');
@@ -8,12 +33,11 @@ let faqCancelIcon = document.getElementsByClassName('faq-cancel-icon');
 
 
 for(let i=0; i < faqAnswer.length; i++){
-    faqItem[i].addEventListener("click", function showAnswer(){;
+    faqItem[i].addEventListener("click", function showAnswer(){
         faqAnswer[i].classList.toggle("d-none");
         faqPlusIcon[i].classList.toggle("d-none");
         faqCancelIcon[i].classList.toggle("d-none");
 })    
-
 
 }
 
